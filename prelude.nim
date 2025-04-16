4ae8701d424c8ece73d539ee680cd2f2e329977e
Vlado Pajić <vlado@brrrrr.at>
2025-04-16 14:18:05 +0200

    update

--- prelude.nim
+++ prelude.nim
@@ -7,13 +7,6 @@
 # This file may not be copied, modified, or distributed except according to
 # those terms.
 
-# import os
-# import strformat
-# const root = currentSourcePath.parentDir
-# const nghttp3LibIncludes    = root/"libs"/"nghttp3"/"lib"/"includes"
-# {.passc: fmt"-I{nghttp3LibIncludes}".}
-
-# {.passL: "-lnghttp3".}
-# {.passC: "-I/usr/local/lib".} 
+# This is auto generated file.
 
 
